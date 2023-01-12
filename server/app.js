c211c851a96381bf481476ec3dd92df6fa773b9a
Shahrear-ahamed <its.shahrear@gmail.com>
2023-01-12 18:53:38 +0600

    initial setup complete

---
+++ server/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.get("/", (req, res) => {
+  res.status(200).send("Hello World!");
+});
+
+// exports the app
+module.exports = app;
